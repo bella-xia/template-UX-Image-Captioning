57b272e02f91a23aea4694e22e56ff6571f63631
bella-xia <zxia15@jh.edu>
2023-08-29 13:53:07 -0400

    added and modified files

--- src/pages/main/main-task1.js
+++ src/pages/main/main-task1.js
@@ -1,184 +1,298 @@
-import React, { Component, useState, useEffect } from "react";
-import {Button, Modal, Checkbox, Input, Radio} from 'antd'
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
 import "antd/dist/antd.css";
 import "./main.css";
 
-import PredictionContainer from '../../components/predictionContainer'
-
 function Main1Container() {
-    const [choice, setChoice] = useState(0);
-    const [imageData, setImageData] = useState([]);
-    const [currentImage, setCurrentImage] = useState("");
-    const [currentPrediction, setCurrentPrediction] = useState("");
-    const [imageCount, setImageCount] = useState(0);
-    const [showPrediction, setShowPrediction] = useState(false);
-    const [taskTime, setTaskTime] = useState((Date.now() + 1000 * 1000));
+  const [currentImage, setCurrentImage] = useState("");
+  const [imageCount, setImageCount] = useState(0);
+  const [taskTime, setTaskTime] = useState(Date.now() + 1000 * 1000);
+  const [taskUseTime, setTaskUseTime] = useState(
+    Array.from({ length: 20 }, (_, i) => 0)
+  );
+  const [totalImages, setTotalImages] = useState(0);
+  const [captions, setCaptions] = useState([
+    "a group of horses standing around a fire",
+    "a group of people standing  around a pool",
+    "a man standing next to a tree by a lake",
+    "a person in a kayak pad in the ocean",
+    "a dog playing in the grass with its tongue",
+    "a man walking down the street",
+    "a woman standing in front of a car",
+    "a man sitting on a bench",
+    "a man standing on a small boat in a river",
+    "a red toy truck",
+    "a group of dogs playing in a fountain",
+    "a person sitting on a dock watching the sunset",
+    "a group of people sitting on a bench in a park",
+    "a couple sitting on a bench",
+    "a dog playing with a water hose in the yard",
+    "a boy on a skateboard",
+    "Two girls playing soccer",
+    "a man parading in the water",
+    "a cat laying in the grass",
+    "a woman in a bikini on a surfboard",
+  ]);
+  // const [captionDict, setCaptionDict] = useState([]);
+  // const [currentTime, setCurrentTime] = useState(0);
+  const [moveToSurvey, setMoveToSurvey] = useState(false);
+  const [render, setRender] = useState(false);
+  const [popUp, setPopUp] = useState(false);
 
-    const [currentTime, setCurrentTime] = useState(0);
-    const [moveToSurvey, setMoveToSurvey] = useState(false);
+  const originalCaptions = [
+    "a group of horses standing around a fire",
+    "a group of people standing  around a pool",
+    "a man standing next to a tree by a lake",
+    "a person in a kayak pad in the ocean",
+    "a dog playing in the grass with its tongue",
+    "a man walking down the street",
+    "a woman standing in front of a car",
+    "a man sitting on a bench",
+    "a man standing on a small boat in a river",
+    "a red toy truck",
+    "a group of dogs playing in a fountain",
+    "a person sitting on a dock watching the sunset",
+    "a group of people sitting on a bench in a park",
+    "a couple sitting on a bench",
+    "a dog playing with a water hose in the yard",
+    "a boy on a skateboard",
+    "Two girls playing soccer",
+    "a man parading in the water",
+    "a cat laying in the grass",
+    "a woman in a bikini on a surfboard",
+  ];
 
-    const [render, setRender] = useState(false);
+  const baseImgUrl = "/image_folder/";
+  const img_paths = Array.from({ length: 20 }, (_, i) => i).map(
+    (idx) => `Image_${idx + 1}.png`
+  );
+  const routeChange = () => {
+    let path = "/#/Survey";
+    window.location.assign(path);
+  };
 
-    let totalImages = 3;
-    const baseImgUrl = "./";
-
-    const routeChange = () =>{ 
-        let path = '/#/Survey'; 
-        window.location.assign(path);
-
+  const updateImage = (count) => {
+    const usedTime = Date.now() - taskTime;
+    setTaskUseTime((l) =>
+      l.map((time, idx) => (idx === imageCount ? usedTime : time))
+    );
+    if (count >= totalImages - 1) {
+      setMoveToSurvey(true);
     }
+    setImageCount(count);
+    setCurrentImage(img_paths[count]);
+    // setCaptionDict(() => [
+    //   {
+    //     idx: 0,
+    //     text: captions[count].substring(0, captions[count].length),
+    //     type: "original",
+    //   },
+    // ]);
+    setTaskTime(Date.now());
+    console.log(taskUseTime);
+  };
 
-    const nextChange = () =>{
-        if (choice<1) {
-            alert("Please make sure to complete all the fields!");
-        } else {
-            let count = imageCount + 1;
-            // save data
-            let data = {
-                q_id: currentImage,
-                user_id: localStorage.getItem("user-id"),
-                ans: choice,
-                time: ((Date.now() - taskTime) / 1000).toFixed(3)
-            };
-            console.log(data)
-            sendData(data)
-            if (count >= totalImages) {
-                console.log('done with images')
-                setMoveToSurvey(true);
-            } else {
-                // reinitialize variables
-                setChoice(0); 
-                setImageCount(count);
-                setCurrentImage(imageData[count].name);
-                setCurrentPrediction(imageData[count].label);
-                setTaskTime(Date.now())
-                setShowPrediction(false);
-            }
-        }
+  const nextChange = () => {
+    // // if (choice < 1) {
+    // //   alert("Please make sure to complete all the fields!");
+    // // } else {
+    //   // save data
+    const count = imageCount + 1;
+    //   let data = {
+    //     q_id: currentImage,
+    //     user_id: localStorage.getItem("user-id"),
+    //     ans: choice,
+    //     time: ((Date.now() - taskTime) / 1000).toFixed(3),
+    //   };
+    //   console.log(data);
+    //   sendData(data);
+    if (captions[imageCount] === originalCaptions[imageCount]) {
+      setPopUp(() => true);
+      return;
     }
+    // reinitialize variables
+    updateImage(count);
+  };
 
-    const sendData = (obj) => {
-        fetch('http://localhost:8080/responsesData', {
-          method: 'POST',
-          body: JSON.stringify(obj),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8"
-          }
-        }).then(response => response.json())
-          .then(message => {
-            console.log(message)
-          })
-      } 
+  const popUpProceed = () => {
+    setPopUp(!popUp);
+    const count = imageCount + 1;
+    if (count >= totalImages) {
+      setMoveToSurvey(true);
+    } else {
+      // reinitialize variables
+      updateImage(count);
+    }
+  };
 
-
-    const onChangeMultiple= e => {
-        setChoice(e.target.value);
-
-    };
-
-    const handlePredict=()=>{
-        setShowPrediction(true);
-    };
-
-    // testing communication with backend
-    useEffect(() => {
-        fetch('http://0.0.0.0:8080/time').then(res => 
-        res.json()).then(data => {
-            setCurrentTime(data.time);
-            console.log(data.time)
-        });
-        }, []);
-    
-
-    // initialize image
-    useEffect(() => {
-        console.log('getting images')
-        fetch('http://localhost:8080/imageInfo')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data['imgs']);
-            setImageData(data['imgs']);
-            let image_name = data['imgs'][0].name
-            setCurrentImage(image_name)
-            console.log(image_name)
-            setCurrentPrediction(data['imgs'][0].label);
-            setRender(true);
-            setTaskTime(Date.now())
-        });
-    }, []);
-
-
-
+  const Popup = (props) => {
     return (
-      <>
-       {render ?
+      <div className="popup-box">
+        <div className="box">{props.content}</div>
+      </div>
+    );
+  };
 
-            <div className="container">
-            <div className="title">Main experiment</div>
-            <div className="column-container"> 
-            <div className="left-column"> 
-                <p> This is how you load an image:</p>
-                <div className="img-frame">
-                    <img className="image-inner" src={baseImgUrl + currentImage}/>
-                </div>
-                <p> {imageCount + 1} / {totalImages} Images</p>
-            </div>
+  const modifyCaption = (modCap) => {
+    // const newCaption = modCap.target.value;
+    // const insert =
+    //   newCaption.length > captions[imageCount].length ? true : false;
+    // const dictLen = captionDict.length;
+    // let j = 0;
+    // for (let i = 0; i < dictLen; i++) {
+    //   let foundElement = captionDict.find((item) => item.idx === i);
+    //   if (foundElement.state === "delete") {
+    //     continue;
+    //   }
+    //   if (newCaption.length < j + 1) {
+    //     foundElement.state = "delete";
 
-            <div className="right-column"> 
-            <p> You can present the outcomes of the algorithms on this side:</p> 
-                
-            
-            <Button className="btn-1"  onClick={()=>{handlePredict()}}>
-                Get a prediction
-            </Button>
+    //     setCaptionDict((dict) =>
+    //       dict.map((item) => (item.idx === i ? foundElement : item))
+    //     );
+    //   }
+    //   if (newCaption[j] !== foundElement.letter) {
+    //     foundElement
+    //   }
+    // }
+    setCaptions(
+      captions.map((caption, idx) =>
+        idx === imageCount ? modCap.target.value : caption
+      )
+    );
+  };
 
-            { showPrediction ?
-                <PredictionContainer 
-                    currentPrediction={currentPrediction}
+  // testing communication with backend
+  //   useEffect(() => {
+  //     fetch("http://0.0.0.0:8080/time")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         setCurrentTime(data.time);
+  //         console.log(data.time);
+  //       });
+  //   }, []);
+
+  // initialize image
+  useEffect(() => {
+    console.log("getting images");
+    setTotalImages(img_paths.length);
+    setCurrentImage(img_paths[imageCount]);
+    setTaskTime(Date.now());
+    setRender(true);
+    // const imageCaption = captions[imageCount];
+    // const data = [];
+    // for (let i = 0; i < imageCaption.length; i++) {
+    //   const letter = imageCaption[i];
+
+    //   // Create a data object for the letter
+    //   const dataObject = {
+    //     idx: i, // Index of the letter
+    //     letter: letter, // The actual letter
+    //     state: "original", // manage the state of the object
+    //   };
+
+    //   // Push the data object to the data array
+    //   data.push(dataObject);
+    // }
+    // setCaptionDict(() => data);
+    console.log();
+  }, []);
+
+  return (
+    <>
+      {render ? (
+        <div className="container">
+          <div className="task_name">Task Instructions: </div>
+          <div className="task_descrip">
+            Correct or accept the AI-generated caption for the fiven image. Make
+            sure that the caption is specific and describes the human action.
+          </div>
+          <div className="column-container">
+            <div className="left-column">
+              <div className="image-frame">
+                <img
+                  className="image-inner"
+                  src={baseImgUrl + currentImage}
+                  alt={currentImage}
                 />
-            :
+              </div>
+              <p>
+                {" "}
+                {imageCount + 1} / {totalImages} Images
+              </p>
+            </div>
+
+            <div className="right-column">
+              <div>
+                <t> AI-generated Original Caption:</t>
+              </div>
+              <input
+                class="caption"
+                value={captions[imageCount]}
+                onChange={modifyCaption}
+              ></input>
+              <div>
+                <t> Final Caption:</t>
+              </div>
+              <input
+                class="caption"
+                value={captions[imageCount]}
+                readOnly={true}
+              ></input>
+            </div>
+          </div>
+
+          <div className="button-container">
+            <Button
+              variant="btn btn-success"
+              style={{ marginLeft: "70%" }}
+              onClick={nextChange}
+              disabled={moveToSurvey}
+            >
+              Next
+            </Button>
+          </div>
+
+          {moveToSurvey && (
+            <div className="button-container">
+              <Button
+                disabled={!moveToSurvey}
+                variant="btn btn-success"
+                onClick={routeChange}
+              >
+                Survey
+              </Button>
+            </div>
+          )}
+          {popUp && (
+            <Popup
+              content={
                 <>
+                  <b>Warning</b>
+                  <p>
+                    You have not made any edit on the AI-generated caption. Do
+                    you want to go to the next image?
+                  </p>
+                  <div className="popbuttons">
+                    <button className="cancel" onClick={() => setPopUp(!popUp)}>
+                      Cancel
+                    </button>
+                    <button className="proceed" onClick={popUpProceed}>
+                      Proceed
+                    </button>
+                  </div>
                 </>
-            }
-
-
-            <div className="instr">
-                <t> This is how you can ask a multiple choice question.</t>
-            </div>    
-                <Radio.Group onChange={onChangeMultiple} value={choice}>
-                    <Radio value={1}> <t> Option 1</t></Radio>
-                    <Radio value={2}> <t> Option 2</t></Radio>
-                    <Radio value={3}> <t> Option 3</t></Radio>
-                </Radio.Group>
-
-            </div>
-            </div>
-
-
-            <div className="button-container"> 
-                <Button variant="btn btn-success"  style={{marginLeft:"70%"}}  onClick={nextChange}>
-                    Next
-                </Button>
-            </div>
-
-            {(moveToSurvey) && 
-            <div className="button-container"> 
-                <Button disabled={!moveToSurvey} variant="btn btn-success" onClick={routeChange}>
-                    Survey
-                </Button>
-            </div>
-            }
-
-            </div>
-
-        :
-            <> 
-            <h1> Loading ...</h1>
-            </>
-        }
-      </>
-       
-      );
+              }
+            />
+          )}
+        </div>
+      ) : (
+        <>
+          <h1> Loading ...</h1>
+        </>
+      )}
+    </>
+  );
 }
 
 export default Main1Container;
--- src/pages/survey/survey.js
+++ src/pages/survey/survey.js
@@ -1,5 +1,5 @@
-import {React, useEffect, useState} from 'react';
-import ReactDOM from 'react-dom';
+import { React, useState } from "react";
+import ReactDOM from "react-dom";
 import { useParams } from "react-router-dom";
 import {
   Form,
@@ -8,116 +8,128 @@ import {
   // Input,
   Button,
   Radio,
-} from 'antd';
-import './survey.css'
+} from "antd";
+import "./survey.css";
 const { Option } = Select;
 
-
-
 const formItemLayout = {
-    labelCol: {
-        span: 22,
-        offset:1
-    },
-    wrapperCol: {
-        span: 30,
-        offset:1
-    },
+  labelCol: {
+    span: 22,
+    offset: 1,
+  },
+  wrapperCol: {
+    span: 30,
+    offset: 1,
+  },
+};
+
+const questions = [
+  "On a scale from 1-5 how accurate do you think your captions were? (1: very inaccurate, 5: very accurate)",
+  "On a scale from 1-5 how accurate do you think the AI generated captions were? (1: very inaccurate, 5: very accurate)",
+  "On a scale from 1-5 how much effort did you put in completing the captioning tasks? (1: minimal effort, 5: a lot of effort)",
+  "On a scale from 1-5 how much of the salient information do you think was covered? (1: small area of coverage, 5: large area of coverage)",
+];
+
+const FormItem = ({ question, idx }) => {
+  const style = { fontSize: "15px", marginLeft: "30px", marginRight: "30px" };
+  return (
+    <Form.Item
+      name={`Q${idx + 1}`}
+      label={
+        <p style={{ fontSize: "18px" }}>
+          {" "}
+          {idx + 1}. {question}
+        </p>
+      }
+      rules={[
+        {
+          required: true,
+        },
+      ]}
+    >
+      <Radio.Group>
+        <Radio value="1" style={style}>
+          1
+        </Radio>
+        <Radio value="2" style={style}>
+          2
+        </Radio>
+        <Radio value="3" style={style}>
+          3
+        </Radio>
+        <Radio value="4" style={style}>
+          4
+        </Radio>
+        <Radio value="5" style={style}>
+          5
+        </Radio>
+      </Radio.Group>
+    </Form.Item>
+  );
 };
 
 const SurveyContainer = () => {
   const [form] = Form.useForm();
   const [answers, setAnswers] = useState({});
 
-
   const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-    let copySaveArray = values
-    setAnswers(values)
+    console.log("Received values of form: ", values);
+    let copySaveArray = values;
+    setAnswers(values);
     // save data
     let data = {
-        user_id: localStorage.getItem("user-id"),
-        q1: 1, 
-        q2: 2,
+      user_id: localStorage.getItem("user-id"),
+      q1: 1,
+      q2: 2,
     };
-    sendData(data)
-    let path = '/#/End'; 
+    sendData(data);
+    let path = "/#/End";
     window.location.assign(path);
   };
 
   const sendData = (obj) => {
-    fetch('http://localhost:8080/surveyData', {
-      method: 'POST',
+    fetch("http://localhost:8080/surveyData", {
+      method: "POST",
       body: JSON.stringify(obj),
       headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    }).then(response => response.json())
-      .then(message => {
-        console.log(message)
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((message) => {
+        console.log(message);
         // getLastestTodos();
-      })
-  } 
-
-
+      });
+  };
 
   return (
-    <div className="container"> 
-      <Form {...formItemLayout} layout='vertical'
+    <div className="container">
+      <Form
+        {...formItemLayout}
+        layout="vertical"
         name="validate_other"
         onFinish={onFinish}
-        initialValues={{
-        }}
+        initialValues={{}}
       >
+        <div className="title">Evaluation Questions</div>
 
-        <div className="title"> Study survey</div>
-        <div className='text'> This is how you can create a questionnaire at the end of the experiment. </div>
+        <div>
+          {questions.map((question, idx) => (
+            <FormItem
+              key={idx}
+              class="form-questions"
+              question={question}
+              idx={idx}
+            />
+          ))}
+        </div>
 
-        <Form.Item 
-            name="Q1" 
-            label = {
-                <p style={{fontSize: "20px"}}> 1. How confident were you in your responses to complete the task?</p>}
-            rules={[{
-                    required: true,
-                  },
-                ]}>
-            <Radio.Group>
-                <Radio value="1" style={{fontSize: "18px"}}>Very unconfident</Radio>
-                <Radio value="2" style={{fontSize: "18px"}}>Unconfident</Radio>
-                <Radio value="3" style={{fontSize: "18px"}}>Average</Radio>
-                <Radio value="4" style={{fontSize: "18px"}}>Confident</Radio>
-                <Radio value="5" style={{fontSize: "18px"}}>Very confident</Radio>
-            </Radio.Group>
-        </Form.Item>
-        
-
-        <Form.Item 
-            name="Q2" 
-            label = {
-                <p style={{fontSize: "20px"}}> 2. How successful do you think you were you in accomplishing what you were asked to do? </p>}
-            rules={[{
-                    required: true,
-                  },
-                ]}>
-            <Radio.Group>
-                <Radio value="1" style={{fontSize: "18px"}}>Poor</Radio>
-                <Radio value="2" style={{fontSize: "18px"}}>Fair</Radio>
-                <Radio value="3" style={{fontSize: "18px"}}>Average</Radio>
-                <Radio value="4" style={{fontSize: "18px"}}>Good</Radio>
-                <Radio value="5" style={{fontSize: "18px"}}>Excellent</Radio>
-            </Radio.Group>
-        </Form.Item>
-
-
-
-         <Form.Item >
-         
-        <Button type="primary" htmlType="submit">
-        Submit
-        </Button>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
         </Form.Item>
       </Form>
-      
     </div>
   );
 };
