dd1807e196af7145ea34f3df4df6e539d699c4ab
Mung Yao Jia <mjia8@jhu.edu>
2024-04-05 21:22:06 -0400

    added attention google forms survey

--- src/pages/eyegazeEnd/eyegazeEnd.js
+++ src/pages/eyegazeEnd/eyegazeEnd.js
@@ -1,21 +1,36 @@
 import { Button } from "antd";
-import React, { Component, useState } from "react";
+import React from "react";
 import "./eyegazeEnd.css";
 
-function eyegazeEndContainer() {
+function EyegazeEndContainer() {
   const routeChange = () => {
     let path = "/#/Survey";
     window.location.assign(path);
   };
 
+  // Retrieve the user-id from localStorage
+  const userId = localStorage.getItem("user-id");
+
   return (
     <div className="container">
       <div className="title">
-        <h1> We will stop recordingM data now. </h1>
-        <Button onClick={routeChange}> Continue </Button>
+        <h1>
+          Please follow this Google Form and fill out questions about the
+          images. Your user ID is {userId ? userId : "not set"}.
+        </h1>
+        <h1>
+          <a
+            href="https://forms.gle/FJn6d9hFXocNCN598"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://forms.gle/FJn6d9hFXocNCN598
+          </a>
+        </h1>
+        <Button onClick={routeChange}>Continue</Button>
       </div>
     </div>
   );
 }
 
-export default eyegazeEndContainer;
+export default EyegazeEndContainer;
